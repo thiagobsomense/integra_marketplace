ddc9038b0add4c0a39f66ff6e4d5a3840a6f7339
thiagobsomense <thiagobsomense@gmail.com>
2023-09-18 17:10:10 -0300

    feat(meli): add access to mercado livre api

--- src/meli/urls.py
+++ src/meli/urls.py
@@ -4,6 +4,6 @@ from .views import auth_meli
 app_name  = 'meli'
 
 urlpatterns = [
-    # path('', auth_meli, name='auth'),
-    path('?<str:code>/?<str:state>/', auth_meli, name='auth'),
+    path('', auth_meli, name='auth'),
+    # path('<str:code>/?<str:state>/', auth_meli, name='auth'),
 ]
--- src/meli/views.py
+++ src/meli/views.py
@@ -3,17 +3,19 @@ from mercadolibre.client import Client
 
 
 def auth_meli(request):
-    CLIENT_ID = '2100982055659711'
-    CLIENT_SECRET = 'svGW52JquU2tBObFjrDU7iQeQqJWOAAO'
+    CLIENT_ID = '5567700473549133'
+    CLIENT_SECRET = 'NFlAtiFUEsf9KauUSxJiRPHXs1hojZBX'
     SITE = 'MLB'
     REDIRECT_URL = 'http://localhost:8000/mercadolivre/'
-    CODE = ''
+    CODE = request.GET.get('code')
 
     client = Client(CLIENT_ID, CLIENT_SECRET, site=SITE)
     url = client.authorization_url(REDIRECT_URL)
-    token = client.exchange_code(REDIRECT_URL, CODE)
-    client.set_token(token)
+    # token = client.exchange_code(REDIRECT_URL, CODE)
+    # client.set_token(token)
 
-    new_token = client.refresh_token()
+    # new_token = client.refresh_token()
+    
+    # print(new_token)
 
-    return render(request, 'teste.html', {'url':url})
+    return render(request, 'teste.html', {'url': url})
